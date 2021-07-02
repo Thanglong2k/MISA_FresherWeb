5536ef4de6f8cd5baa8bbe638ab9998d2d22fca5
Thanglong2k <thanglong2k@gmail.com>
2021-07-02 14:06:27 +0700

    project 2/7/2021

---
+++ js/common/base.js
@@ -0,0 +1,39 @@
+﻿
+
+class BaseJS {
+    constructor() {
+        this.loadData();
+    }
+
+    loadData() {
+        //lấy thông tin các cột dữ liệu
+
+        var ths = $("table tr th");
+        var fieldNames = [];
+        
+        //lấy dữ liệu về
+        $.ajax({
+            url: "http://api.manhnv.net/api/employees",
+
+            method: "GET"
+        }).done(function (res) {//chạy ko có lỗi, res là data khi sử dụng postman nó trả về res
+            $.each(res, function (index,obj) {
+                var tr = $(`<tr></tr>`);
+                //lấy thông tin dữ liệu sẽ map tương ứng với các cột:
+                $.each(ths, function (index, th) {
+                    var td = `<td><div><span></span></div></td>`;
+                    var fieldName = $(th).attr("fieldName");
+                    var value = obj.fieldName;
+                    td.append(value);
+                    $(tr).append(td);
+                })
+                $("table tbody").append(tr);
+            })
+
+
+        }).fail(function (res) {//chạy có lỗi
+
+        })
+    //binding dữ liệu lên table
+    }
+}
---
+++ js/page/employee.js
@@ -0,0 +1,76 @@
+﻿$(document).ready(function () {
+    new EmployeeJS();
+});
+/** *******************************************
+ * Class quản lý sự kiện cho trang Employee
+ * Created by : TTLONG (2/7/2021)
+ * */
+class EmployeeJS extends BaseJS{
+    constructor() {
+        super();
+        /*this.loadData();*/
+    }
+    /**
+     * Load dữ liệu
+     * Created by : TTLONG (1/7/2021)
+     * */
+    /*loadData() {
+    //lấy dữ liệu về
+    $.ajax({
+        url: "http://api.manhnv.net/api/employees",
+        method: "GET"
+    }).done(function (res) {//chạy ko có lỗi, res là data khi sử dụng postman nó trả về res
+        var data = res;
+
+
+        $.each(data, function (index, item) {
+            var dateOfBirth = item.DateOfBirth;
+            var salary = item.Salary;
+            dateOfBirth = formatDate();
+            salary = formatMoney(15000000);
+            var tr = $(`<tr>
+                            <td>`+ item.EmployeeCode + `</td>
+                            <td>`+ item.FullName + `</td>
+                            <td>`+ item.GenderName + `</td>
+                            <td class="text-align-center">`+ dateOfBirth + `</td>
+                            <td>`+ item.PhoneNumber + `</td>
+                            <td>`+ item.Email + `</td>
+                            <td>`+ item.PositionName + `</td>
+                            <td>`+ item.DepartmentName + `</td>
+                            <td class="text-align-right">`+ salary + `</td>
+                            <td style="max-width:250px" title="`+ item.Address + `">` + item.Address + `</td>
+                            <td>`+ item.WorkStateName + `</td>
+                            
+
+                        </tr >`);
+            $("tbody").append(tr);
+        })
+    }).fail(function (res) {//chạy có lỗi
+
+    })
+    //binding dữ liệu lên table
+
+} */
+    /**
+    * Thêm dữ liệu
+    * Created by : TTLONG (2/7/2021)
+    * */
+    add() {
+
+    }
+    /**
+    * Sửa dữ liệu
+    * Created by : TTLONG (2/7/2021)
+    * */
+    edit() {
+
+    }
+    /**
+    * Xóa dữ liệu
+    * Created by : TTLONG (2/7/2021)
+    * */
+    delete() {
+
+    }
+}
+
